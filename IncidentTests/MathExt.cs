a7f7ba13b8426b9886b0e672f3755ff12a0afdb8
kornelijepetak <kornelije@gmail.com>
2015-09-09 06:44:07 +0200

    Add tests into solution

---
+++ IncidentTests/MathExt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentTests
+{
+	public static class MathExt
+	{
+		public static double StdDev(this int[] values)
+		{
+			var mean = values.Average();
+
+			var variance = values
+				.Select(x => Math.Pow(mean - x, 2))
+				.Average();
+
+			return Math.Sqrt(variance);
+		}
+
+		public static bool Validate(this int[] values, int numbers, double upperBoundForStdDev)
+		{
+			var stdDev = values.StdDev();
+
+			// Normalize percentage
+			upperBoundForStdDev /= 100.0;
+
+			var expectedCountInBucket = 1.0 * numbers / values.Length;
+
+			var relativeStdDev = stdDev / expectedCountInBucket;
+
+			return relativeStdDev < upperBoundForStdDev;
+
+			//return sigmaTooHightPercentage <= allowedPercentage;
+		}
+
+	}
+}
---
+++ IncidentTests/Primitives.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KornelijePetak.IncidentCS;
+
+namespace IncidentTests
+{
+	[TestClass]
+	public class Primitives
+	{
+		public const int DefaultTestIterationCount = 10000000;
+
+		[TestMethod]
+		public void BooleanDistribution()
+		{
+			Test(() => Incident.Primitive.Boolean ? 1 : 0, 2);
+		}
+
+		[TestMethod]
+		public void ByteDistribution()
+		{
+			Test(() => Incident.Primitive.Byte, 256);
+		}
+
+		[TestMethod]
+		public void ByteBetweenDistribution()
+		{
+			// Test distribution
+			Test(() => Incident.Primitive.ByteBetween(15, 25) - 15, 10);
+
+			// Test that there are no numbers outside the interval
+			for (int i = 0; i < DefaultTestIterationCount; i++)
+			{
+				var rnd = Incident.Primitive.ByteBetween(15, 25);
+				Assert.IsTrue(15 <= rnd && rnd < 25);
+			}
+
+			// Test inversion
+			for (int i = 0; i < DefaultTestIterationCount; i++)
+			{
+				var rnd = Incident.Primitive.ByteBetween(25, 15);
+				Assert.IsTrue(15 <= rnd && rnd < 25);
+			}
+		}
+
+		[TestMethod]
+		public void ByteBetweenOutOfInterval()
+		{
+			// Test that there are no numbers outside the interval
+			for (int i = 0; i < DefaultTestIterationCount; i++)
+			{
+				var rnd = Incident.Primitive.ByteBetween(15, 25);
+				Assert.IsTrue(15 <= rnd && rnd < 25);
+			}
+		}
+
+		[TestMethod]
+		public void ByteBetweenInversion()
+		{
+			for (int i = 0; i < DefaultTestIterationCount; i++)
+			{
+				var rnd = Incident.Primitive.ByteBetween(25, 15);
+				Assert.IsTrue(15 <= rnd && rnd < 25);
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void ByteBetweenThrows()
+		{
+			Incident.Primitive.ByteBetween(256, 300);
+		}
+
+		public void Test(Func<int> nextRandomElement, int arraySize, int numberCount = DefaultTestIterationCount, double expectedPercentage = 1)
+		{
+			int[] counts = new int[arraySize];
+
+			for (int i = 0; i < numberCount; i++)
+				counts[nextRandomElement()]++;
+
+			// Expect that standard deviation is smaller than expectedPercentage% of the expected bucket size
+			Assert.IsTrue(counts.Validate(numberCount, expectedPercentage));
+		}
+	}
+}
