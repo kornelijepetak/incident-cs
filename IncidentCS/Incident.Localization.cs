31c05ee6356e1e08f175c33f66af69462f67e106
kornelijepetak <kornelije@gmail.com>
2015-09-09 20:23:26 +0200

    Add basic support for localization

---
+++ IncidentCS/Incident.Localization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace KornelijePetak.IncidentCS
+{
+	public static partial class Incident
+	{
+		private static CultureInfo culture;
+
+		public static String Culture
+		{
+			get
+			{
+				if (culture == null)
+					culture = CultureInfo.CurrentCulture;
+
+				return culture.Name;
+			}
+			set
+			{
+				try
+				{
+					culture = new CultureInfo(value);
+				}
+				catch 
+				{
+					culture = null;
+				}
+			}
+		}
+	}
+}
