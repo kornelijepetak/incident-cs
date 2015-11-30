b5b6665e964eb02343c226f15abcb542b0090ed7
kornelijepetak <kpetak@koncar-inem.hr>
2015-11-30 12:10:31 +0100

    Add .NET 3.5 version to the repository

--- Incident35/Properties/AssemblyInfo.cs
+++ Incident35/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Incident")]
+[assembly: AssemblyTitle("Incident35")]
 [assembly: AssemblyDescription("A world of everything random")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Incident")]
+[assembly: AssemblyProduct("Incident35")]
 [assembly: AssemblyCopyright("Copyright ©  2015 Kornelije Petak")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("3d979b62-8988-4740-97d3-fca97e9f0277")]
+[assembly: Guid("2c059a5e-7969-4250-94e3-7d39f2c45424")]
